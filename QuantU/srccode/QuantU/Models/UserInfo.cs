c4e53e0f8cc9ff5fe175b02931ded9e4884153dc
DonovanBrown <donovanasherb@gmail.com>
2023-03-18 18:13:14 -0500

    Documentation
    Added comment documentation throughout the file, both for encryption and the constructor

--- QuantU/srccode/QuantU/Models/UserInfo.cs
+++ QuantU/srccode/QuantU/Models/UserInfo.cs
@@ -7,6 +7,11 @@ using System.Security.Cryptography;
 namespace QuantU.Models{
     public class UserInfo {
 
+        /*  This class contains all info related to the User class objects.
+            The code section below is both the constructor and initialization of class fields
+            The BSON elemnts allow the field below it to be referred to as something else later in the code, used in the Index of the User View
+            UserInfo items are encrypted and stored in an external Mongo database
+        */  
         public string username {get; set;} = null!;
         public string email {get; set;} = null!;
         public string password {get; set;} = null!;
@@ -17,6 +22,13 @@ namespace QuantU.Models{
 
 
 
+        /*  This function takes in a UserInfo object and hashes the data before it is stored
+            This function is called in the UserController before the user is pushed to the database
+            The data hashed is the password and the recovery question answer
+            Inputs: a UserInfo object
+            Outputs: a UserInfo object
+            This code can likely be optimized later but I didn't find it to be a priority yet
+        */
     public static UserInfo HashingAlgo(UserInfo user) {
 
          using (SHA256 sha256Hash = SHA256.Create())  {  
@@ -45,8 +57,23 @@ namespace QuantU.Models{
     return user;
     }
 
-    static String encrpyt = "gu4vajuic3keyb0ard86";
 
+
+    /*  This is the encryption key. It probably should not be stored in plaintext but here we are
+        If this key is changed, be aware that none of the encrpyted data can be accessed until the string is changed back
+        DO NOT CHANGE THE STRING WITHOUT CONSULTING SOMEBODY
+    */
+    static readonly String encrpyt = "gu4vajuic3keyb0ard86";
+
+
+
+    /*  This function takes in a UserInfo object and encrypts the data before it is stored
+        This function is called in the UserController before the user is pushed to the database
+        The data encrypted is the username, email, and recovery question
+        Inputs: a UserInfo object (also uses the encrypt string)
+        Outputs: a UserInfo object
+        This code can likely be optimized later but I didn't find it to be a priority yet
+    */
     public static UserInfo EncryptAlgo(UserInfo user) {
         StringBuilder buildUser = new StringBuilder();
         StringBuilder buildRecovery = new StringBuilder();
@@ -81,6 +108,13 @@ namespace QuantU.Models{
     }
 
 
+
+    /*  This function takes in an encrypted UserInfo object and decrypts its username to be used/displayed
+        As of creation, the code is not implemented anywhere but it has been tested
+        The function only decrypts the username
+        Inputs: a UserInfo object (also uses the encrypt string)
+        Outputs: a string
+    */
     public static string DecryptUsername(UserInfo user) {
         StringBuilder DecryptedUser = new StringBuilder();
         while(encrpyt.Length < user.username.Length) {
@@ -94,6 +128,14 @@ namespace QuantU.Models{
         return DecryptedUser.ToString();
     }
 
+
+
+    /*  This function takes in an encrypted UserInfo object and decrypts its email to be used/displayed
+        As of creation, the code is not implemented anywhere but it has been tested
+        The function only decrypts the email
+        Inputs: a UserInfo object (also uses the encrypt string)
+        Outputs: a string
+    */
     public static string DecryptEmail(UserInfo user) {
         StringBuilder DecryptedEmail = new StringBuilder();
         while(encrpyt.Length < user.email.Length) {
@@ -107,6 +149,14 @@ namespace QuantU.Models{
         return DecryptedEmail.ToString();
     }
 
+
+
+    /*  This function takes in an encrypted UserInfo object and decrypts its recovery question to be used/displayed
+        As of creation, the code is not implemented anywhere but it has been tested
+        The function only decrypts the recovery question
+        Inputs: a UserInfo object (also uses the encrypt string)
+        Outputs: a string
+    */
     public static string DecryptRecovery(UserInfo user) {
         StringBuilder DecryptedRecovery = new StringBuilder();
         while(encrpyt.Length < user.recoveryQ.Length) {
