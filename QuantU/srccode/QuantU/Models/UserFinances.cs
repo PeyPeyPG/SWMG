a54a857c1534c6f0da1ba237ec1fba1cfde562de
Aidanod123 <aidanod123@gmail.com>
2023-03-19 20:34:47 -0500

    user quiz finished

--- QuantU/srccode/QuantU/Models/UserFinances.cs
+++ QuantU/srccode/QuantU/Models/UserFinances.cs
@@ -10,7 +10,11 @@ namespace QuantU.Models
         public string? username { get; set; }
         public double? netSalary { get; set; }
         public string? maritalStatus { get; set; }
-        public string? interests { get; set; }
+        public List<string>? interests { get; set; }
         public int? age { get; set; }
+
+        public bool TechnologyChecked { get; set; }
+        public bool BankingChecked { get; set; }
+        public bool AutomotiveChecked { get; set; }
     }
 }
