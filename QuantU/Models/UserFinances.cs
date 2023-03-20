524df5fa9aae82ea6a6cc052bb85c4cafea4c836
Aidanod123 <aidanod123@gmail.com>
2023-03-19 20:47:50 -0500

    User quiz is complete

--- QuantU/Models/UserFinances.cs
+++ QuantU/Models/UserFinances.cs
@@ -12,5 +12,8 @@ namespace QuantU.Models
         public string? maritalStatus { get; set; }
         public string? interests { get; set; }
         public int? age { get; set; }
+         public bool TechnologyChecked { get; set; }
+        public bool BankingChecked { get; set; }
+        public bool AutomotiveChecked { get; set; }
     }
 }
