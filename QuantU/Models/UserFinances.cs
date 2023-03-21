837d2dc0f42635805af9d79c643d44a67a6a1e05
Aidanod123 <aidanod123@gmail.com>
2023-03-20 21:08:07 -0500

    Fixed user quiz and added documentation
    Added documentation to UserInfo UserFinances and User/Index, UserQuiz view
    for some reason there is no Login Page

--- QuantU/Models/UserFinances.cs
+++ QuantU/Models/UserFinances.cs
@@ -7,18 +7,27 @@ namespace QuantU.Models
 {
     public class UserFinances
     {
+
         public string? username { get; set; }
         public double? netSalary { get; set; }
         public string? maritalStatus { get; set; }
         public string? interests { get; set; }
         public int? age { get; set; }
+
+        //User Interests checkboxes
         public bool TechnologyChecked { get; set; }
         public bool BankingChecked { get; set; }
         public bool AutomotiveChecked { get; set; }
-        public List<List<StockData>>? portfolioList { get; set; }
-    
 
-        public UserFinances(string username){
+        public List<List<StockData>>? portfolioList { get; set; }
+
+        //constructor for null values
+        public UserFinances()
+        {}
+
+        //peytons shit
+        public UserFinances(string username)
+        {
             this.username = username;
             this.portfolioList = new List<List<StockData>>();
         }
